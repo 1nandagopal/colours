7204410b78c82fd3853610989baf784ca2cc85bc
Nandagopal S <168453056+1nandagopal@users.noreply.github.com>
2024-05-03 22:18:38 +0530

    Added dropdown feature to enable users to select different colour format

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,8 +3,20 @@ import Slider from "rc-slider";
 import "rc-slider/assets/index.css";
 
 import "./Navbar.css";
+import { MenuItem, Select } from "@mui/material";
 
 export default class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      colourFormat: "hex",
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(e) {
+    this.setState({ colourFormat: e.target.value });
+    this.props.changeColourFormat(e.target.value);
+  }
   render() {
     return (
       <header className="Navbar">
@@ -22,6 +34,13 @@ export default class Navbar extends Component {
               onChange={this.props.changeLevel}
             />
           </div>
+        </div>
+        <div className="select-container">
+          <Select value={this.state.colourFormat} onChange={this.handleChange}>
+            <MenuItem value="hex">HEX</MenuItem>
+            <MenuItem value="rgb">RGB</MenuItem>
+            <MenuItem value="rgba">RGBA</MenuItem>
+          </Select>
         </div>
       </header>
     );
--- src/Palette.js
+++ src/Palette.js
@@ -7,22 +7,34 @@ import Navbar from "./Navbar";
 export default class Palette extends Component {
   constructor(props) {
     super(props);
-    this.state = { level: 500 };
+    this.state = { level: 500, colourFormat: "hex" };
     this.changeLevel = this.changeLevel.bind(this);
+    this.changeColourFormat = this.changeColourFormat.bind(this);
   }
 
   changeLevel(level) {
-    console.log(level);
     this.setState({ level });
   }
 
+  changeColourFormat(newFormat) {
+    this.setState({ colourFormat: newFormat });
+  }
   render() {
     const colourBoxes = this.props.palette.colours[this.state.level].map(
-      (colour) => <ColourBox background={colour.hex} name={colour.name} />
+      (colour) => (
+        <ColourBox
+          background={colour[this.state.colourFormat]}
+          name={colour.name}
+        />
+      )
     );
     return (
       <div className="Palette">
-        <Navbar changeLevel={this.changeLevel} level={this.state.level} />
+        <Navbar
+          changeLevel={this.changeLevel}
+          level={this.state.level}
+          changeColourFormat={this.changeColourFormat}
+        />
         <div className="Palette-colours">{colourBoxes}</div>
       </div>
     );
