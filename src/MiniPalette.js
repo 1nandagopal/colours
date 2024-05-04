19d0b16392a763c405d44fd69d69e52f19dcf6c2
Nandagopal S <168453056+1nandagopal@users.noreply.github.com>
2024-05-04 22:34:50 +0530

    Styled PaletteList. Created a styled MiniPalette.js

---
+++ src/MiniPalette.js
@@ -0,0 +1,69 @@
+import { Component } from "react";
+import { withStyles } from "@mui/styles";
+
+const style = {
+  root: {
+    backgroundColor: "white",
+    border: "1px solid black",
+    borderRadius: "5px",
+    padding: "0.5rem",
+    position: "relative",
+    overflow: "hidden",
+    "&:hover": {
+      cursor: "pointer",
+    },
+  },
+  colours: {
+    backgroundColor: "#dae1e4",
+    height: "150px",
+    width: "100%",
+    borderRadius: "5px",
+    overflow: "hidden",
+  },
+  title: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    margin: "0",
+    color: "black",
+    paddingTop: "0.5rem",
+    fontSize: "1rem",
+    position: "relative",
+  },
+  emoji: {
+    marginLeft: "0.5rem",
+    fontSize: "1.5rem",
+  },
+  miniColour: {
+    height: "25%",
+    width: "20%",
+    display: "inline-block",
+    margin: "0 auto",
+    position: "relative",
+    marginBottom: "-3.5px",
+  },
+};
+
+class MiniPalette extends Component {
+  render() {
+    const { classes, paletteName, emoji, colours } = this.props;
+    const miniColorBoxes = colours.map((colour) => (
+      <div
+        className={classes.miniColour}
+        style={{ backgroundColor: colour.colour }}
+        key={colour.name}
+      />
+    ));
+
+    return (
+      <div className={classes.root}>
+        <div className={classes.colours}>{miniColorBoxes}</div>
+        <h5 className={classes.title}>
+          {paletteName} <span className={classes.emoji}>{emoji}</span>
+        </h5>
+      </div>
+    );
+  }
+}
+
+export default withStyles(style)(MiniPalette);
