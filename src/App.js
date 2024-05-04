9420f2a07454a9a538447fff8832c497dbd67a63
Nandagopal S <168453056+1nandagopal@users.noreply.github.com>
2024-05-04 12:57:56 +0530

    Started adding routing features

--- src/App.js
+++ src/App.js
@@ -2,13 +2,31 @@ import { Component } from "react";
 import Palette from "./Palette";
 import SeedColours from "./SeedColours";
 import { generatePalette } from "./colourHelpers";
+import { Route, Switch } from "react-router-dom";
 
 export default class App extends Component {
+  findPalette(id) {
+    return SeedColours.find((palette) => palette.id === id);
+  }
   render() {
     return (
-      <div>
-        <Palette palette={generatePalette(SeedColours[4])} />
-      </div>
+      <Switch>
+        <Route exact path="/" render={() => <h1>Hi</h1>} />
+        <Route
+          exact
+          path="/palette/:paletteId"
+          render={(routeProps) => (
+            <Palette
+              palette={generatePalette(
+                this.findPalette(routeProps.match.params.paletteId)
+              )}
+            />
+          )}
+        />
+      </Switch>
+      // <div>
+      //   <Palette palette={generatePalette(SeedColours[4])} />
+      // </div>
     );
   }
 }
